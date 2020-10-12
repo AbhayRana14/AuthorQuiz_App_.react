0367cd899533ea7e41da410739dea413306eb77f
Abhay Rana <14abhayrana@gmail.com>
2020-10-13 00:06:10 +0530

    ok

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+
+
+class AuthoQuiz extends Component{
+  render(){
+    return(
+      <div>Author Quiz</div>
+    );
+  }
+}
+
+export default AuthoQuiz;
